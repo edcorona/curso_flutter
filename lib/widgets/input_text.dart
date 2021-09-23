f6cc1a05476b7fede5b3893a2f338a84443f5cc5
unknown <lev.corona@gmail.com>
2021-09-23 17:29:35 -0500

    mejoras

--- lib/widgets/input_text.dart
+++ lib/widgets/input_text.dart
@@ -4,12 +4,13 @@ import 'package:flutter/material.dart';
 class inputText extends StatelessWidget {
   final String label;
   final TextInputType keyboardType;
-  final bool oscureText;
+  final bool oscureText, borderEnabled;
   const inputText({
     Key? key,
     this.label = '',
     this.keyboardType = TextInputType.text,
     this.oscureText = false,
+    this.borderEnabled = true,
   }) : super(key: key);
 
   @override
@@ -19,6 +20,12 @@ class inputText extends StatelessWidget {
       obscureText: this.oscureText,
       decoration: InputDecoration(
         labelText: this.label,
+        contentPadding: EdgeInsets.symmetric(vertical: 5),
+        enabledBorder: this.borderEnabled
+            ? UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.black12),
+              )
+            : InputBorder.none,
         labelStyle: TextStyle(
           color: Colors.black45,
           fontWeight: FontWeight.w500,
--- lib/widgets/login_form.dart
+++ lib/widgets/login_form.dart
@@ -3,6 +3,7 @@ import 'dart:html';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'input_text.dart';
+import 'package:flutter_api_rest/utils/responsive.dart';
 
 class LoginForm extends StatefulWidget {
   @override
@@ -12,6 +13,8 @@ class LoginForm extends StatefulWidget {
 class _loginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
+    final Responsive responsive = Responsive.of(context);
+
     return Positioned(
       bottom: 30,
       left: 20,
@@ -22,10 +25,70 @@ class _loginFormState extends State<LoginForm> {
             keyboardType: TextInputType.emailAddress,
             label: "EMAIL, ADDRESS",
           ),
-          inputText(
-            label: "PASSWORD",
-            oscureText: true,
+          Container(
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.black12,
+                ),
+              ),
+            ),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: inputText(
+                    label: "PASSWORD",
+                    oscureText: true,
+                    borderEnabled: false,
+                  ),
+                ),
+                FlatButton(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Forgot Password",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
+          SizedBox(height: responsive.dp(5)),
+          SizedBox(
+            width: double.infinity,
+            child: FlatButton(
+              padding: EdgeInsets.symmetric(vertical: 15),
+              onPressed: () {},
+              child: Text(
+                "Sing in",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Colors.pinkAccent,
+            ),
+          ),
+          SizedBox(height: responsive.dp(2)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "New to friendly Desi?",
+                style: TextStyle(
+                  fontSize: responsive.dp(1.6),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(
+                    color: Colors.pinkAccent,
+                    fontSize: responsive.dp(1.6),
+                  ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 100),
         ],
       ),
     );
